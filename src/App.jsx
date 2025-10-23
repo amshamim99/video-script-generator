d75a9ceff2240157105855b4670b7246e0398412
shamim <amshamim99@gmail.com>
2025-10-23 20:39:24 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+import Navbar from './component/Navbar'
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+    </>
+  )
+}
+
+export default App
---
+++ src/component/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { GoArrowRight } from "react-icons/go";
+const Navbar = () => {
+    return (
+        <>
+            <header className='vdo-font-bold vdo-p-10'>
+                <div className='vdo-container vdo-mx-auto vdo-flex vdo-items-center vdo-justify-between'>
+                    <a className='vdo-text-3xl' href="#">Video Script Generator</a>
+                    <nav>
+                        <ul className='vdo-flex vdo-gap-7'>
+                            <li><a className='vdo-text-[22px] vdo-font-normal vdo-transition-all vdo-duration-300 hover:vdo-text-purple-500' href="#">Home</a></li>
+                            <li><a className='vdo-text-[22px] vdo-font-normal vdo-transition-all vdo-duration-300 hover:vdo-text-purple-500' href="#">About</a></li>
+                            <li><a className='vdo-text-[22px] vdo-font-normal vdo-transition-all vdo-duration-300 hover:vdo-text-purple-500' href="#">Product</a></li>
+                            <li><a className='vdo-text-[22px] vdo-font-normal vdo-transition-all vdo-duration-300 hover:vdo-text-purple-500' href="#">Contact</a></li>
+                        </ul>
+                    </nav>
+                    <button className='vdo-bg-purple-500 vdo-py-3 vdo-px-5 vdo-text-white vdo-flex vdo-items-center vdo-gap-2 vdo-text-base vdo-rounded'>Login <GoArrowRight /></button>
+                </div>
+            </header>
+        </>
+    )
+}
+
+export default Navbar
